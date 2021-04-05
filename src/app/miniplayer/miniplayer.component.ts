53614a3b846d73f993b9ed6db2103bc9635cba4a
Nirmal-Scrapper <thalaivar438@gmail.com>
2021-04-05 18:41:33 +0530

    corrected-song-bar

--- src/app/miniplayer/miniplayer.component.ts
+++ src/app/miniplayer/miniplayer.component.ts
@@ -6,101 +6,71 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./miniplayer.component.css']
 })
 export class MiniplayerComponent implements OnInit {
-  ballanim: Animation;
   constructor() { }
   index = 0;
+  alt="";
   playNow(song: any) {
     //this.ballanim = undefined;
     let ball = document.getElementById("barball");
     var temp = ball.getAnimations();
-
-
     document.getElementById("songname").innerHTML = song.name;
-    document.getElementById("play").innerHTML = "play_arrow";
-    this.playPause();
+    document.getElementById("play").innerHTML = "pause";
+    ball.classList.remove("ballanime");
+    ball.style.animationPlayState="running";
+    this.player();
+    this.alt=song.name;
+    document.getElementById("songpic").innerHTML='<img src='+ song.image+'  width="50" height="50" alt='+song.name+'>'
   }
   playPause() {
     let ball = document.getElementById("barball");
     var temp = ball.getAnimations();
-    //ball.remove();
-
     if (document.getElementById("songname").innerHTML == "") {
       return;
     }
-    console.log(this.ballanim)
-    if (temp.length == 0) {
-      this.ballanim = this.player();
-      //console.log("1", this.ballanim)
-    }
-    // else{
-    //   console.log(this.ballanim.id);
-    // }
     var play = document.getElementById("play").innerHTML;
     console.log(temp[0]);
     console.log(ball.style);
     if (play == "play_arrow") {
       document.getElementById("play").innerHTML = "pause";
-      // console.log(this.ballanim);
-      // console.log("2", this.ballanim)
-      //this.ballanim.play();
-      temp[0].play();
+      //temp[0].play();
+      ball.style.animationPlayState="running";
     }
     else {
       document.getElementById("play").innerHTML = "play_arrow";
-      temp[0].pause();
-      // console.log("3", this.ballanim)
+      //temp[0].pause();
+      ball.style.animationPlayState="paused";
+
     }
   }
 
-  player(): Animation {
+  player() {
     var from: any;
     let ball = document.getElementById("barball");
     var to = (document.getElementById('bar').offsetWidth);
     from = 0;
     console.log(from, to);
-    var temp = ball.getAnimations();
-   // ball.style.
-    if (temp.length == 0) {
-      this.ballanim = ball.animate([
-        { left: from + 'px' },
-        { left: to + 'px' }
-      ], {
-        duration: 50000,
-        fill: 'forwards',
-        easing: 'linear',
-      });
-    }
 
-//     var keyFrames = '\
-// @keyframes ballmove {\
-//   0%{\
-// left:'+ from + 'px\
-//   }\
-//     100% {\
-//       left:'+ to + 'px\
-//     }\
-// }\
-// }';
-// this.writeStyles(keyFrames);
-//     this.writeStyles(".ballanime{ \
-//       animation-name: ballmove;\
-//   animation-duration: 10s;\
-//   animation-fill-mode: forwards;\
-//   animation-play-state: running;\
-//   animation-timing-function: linear;\
-// }");
-//   ball.classList.add("ballanime");
-    if (this.ballanim.id == "") {
-      const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-      let result = ' ';
-      const charactersLength = characters.length;
-      for (let i = 0; i < 5; i++) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
-      }
-      this.ballanim.id = result;
-    }
-    console.log(this.ballanim.id);
-    return this.ballanim;
+
+    var keyFrames = '\
+                    @keyframes ballmove {\
+                      0%{\
+                    left:'+ from + 'px\
+                      }\
+                        100% {\
+                          left:'+ to + 'px\
+                        }\
+                    }\
+                    }';
+      this.writeStyles(keyFrames);
+      this.writeStyles(".ballanime{ \
+                            animation-name: ballmove;\
+                            animation-duration: 100s;\
+                            animation-fill-mode: forwards;\
+                            animation-play-state: running;\
+                            animation-timing-function: linear;\
+          }");
+      ball.classList.add("ballanime");
+    
   }
 
   writeStyles(cssText) {
